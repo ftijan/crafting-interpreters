6de879a4aaf68afc57e9ec98ac8f3ae495d79f1e
ftijan <41797259+ftijan@users.noreply.github.com>
2021-04-02 03:57:55 +0100

    resolving and binding (#8)

--- src/com/craftinginterpreters/lox/Environment.java
+++ src/com/craftinginterpreters/lox/Environment.java
@@ -42,4 +42,21 @@ public class Environment {
 
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
+
+    public Object getAt(Integer distance, String name) {
+        return ancestor(distance).values.get(name);
+    }
+
+    private Environment ancestor(int distance) {
+        Environment environment = this;
+        for (int i = 0; i < distance; i++) {
+            environment = environment.enclosing;            
+        }
+
+        return environment;
+    }
+
+    public void assignAt(Integer distance, Token name, Object value) {
+        ancestor(distance).values.put(name.lexeme, value);
+    }
 }
