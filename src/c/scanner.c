9dc0b559c46960b9324e799bec705734bbbe674c
Filip Tijan <ftijan@gmail.com>
2022-10-29 17:31:53 +0100

    scan token bugfix

--- src/c/scanner.c
+++ src/c/scanner.c
@@ -193,10 +193,10 @@ Token scanToken() {
         case '+': return makeToken(TOKEN_PLUS);
         case '/': return makeToken(TOKEN_SLASH);
         case '*': return makeToken(TOKEN_STAR);
-        case '!': return makeToken(match('=' ? TOKEN_BANG_EQUAL : TOKEN_BANG));
-        case '=': return makeToken(match('=' ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL));
-        case '<': return makeToken(match('=' ? TOKEN_LESS_EQUAL : TOKEN_LESS));
-        case '>': return makeToken(match('=' ? TOKEN_GREATER_EQUAL : TOKEN_GREATER));
+        case '!': return makeToken(match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+        case '=': return makeToken(match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+        case '<': return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+        case '>': return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
         case '"': return string();
     }
 
