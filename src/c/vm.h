3eab75b178d9b26e1cd343d0578c481992e4d420
ftijan <41797259+ftijan@users.noreply.github.com>
2022-10-31 01:09:46 +0000

    adding clox functions (#22)
    * add function base with call stack
    * compiler stack, fn params
    * handle fun calls
    * return statement in fns
    * binding to native functions

--- src/c/vm.h
+++ src/c/vm.h
@@ -1,15 +1,23 @@
 #ifndef clox_vm_h
 #define clox_vm_h
 
-#include "chunk.h"
+#include "object.h"
 #include "table.h"
 #include "value.h"
 
+#define FRAMES_MAX 64
 #define STACK_MAX 256
 
 typedef struct {
-    Chunk* chunk;
+    ObjFunction* function;
     uint8_t* ip;
+    Value* slots;
+} CallFrame;
+
+typedef struct {
+    CallFrame frames[FRAMES_MAX];
+    int frameCount;
+
     Value stack[STACK_MAX];
     Value* stackTop;
     Table globals;
