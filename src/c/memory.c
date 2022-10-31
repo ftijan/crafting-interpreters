38bba26337e590d40c96273f7f98e74a1395025d
Filip Tijan <ftijan@gmail.com>
2022-10-31 13:38:45 +0000

    add gc base

--- src/c/memory.c
+++ src/c/memory.c
@@ -3,7 +3,18 @@
 #include "memory.h"
 #include "vm.h"
 
+#ifdef DEBUG_LOG_GC
+#include <stdio.h>
+#include "debug.h"
+#endif
+
 void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
+    if (newSize > oldSize) {
+#ifdef DEBUG_STRESS_GC
+        collectGarbage();
+#endif
+    }
+    
     if (newSize == 0) {
         free(pointer);
         return NULL;
@@ -15,6 +26,10 @@ void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
 }
 
 static void freeObject(Obj* object) {
+#ifdef DEBUG_LOG_GC
+    printf("%p free type %d\n", (void*)object, object->type);
+#endif
+
     switch (object->type)
     {
         case OBJ_CLOSURE: {
@@ -44,6 +59,16 @@ static void freeObject(Obj* object) {
             break;
         }
     }
+}
+
+void collectGarbage() {
+#ifdef DEBUG_LOG_GC
+    printf("-- gc begin\n");
+#endif
+
+#ifdef DEBUG_LOG_GC
+    printf("-- gc end\n");
+#endif
 }
 
 void freeObjects() {
--- src/c/memory.h
+++ src/c/memory.h
@@ -20,6 +20,7 @@
         sizeof(type) * (newCount))
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize);
+void collectGarbage();
 void freeObjects();
 
 #endif
